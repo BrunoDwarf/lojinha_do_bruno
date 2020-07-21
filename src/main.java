66cb75413cb2c888d1269c2ae00f5cc4c48ebe2d
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2020-07-21 17:49:07 -0300

    Cadastro Categoria e Produto - parte 1.

--- src/main.java
+++ src/main.java
@@ -8,26 +8,26 @@
  */
 
 import corejava.Console1;
-import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        int qnt_prod, qnt_cat, opcao_menu, opcao, desconto_continuo, taxa_emergencial;
-        String produto_nome, categoria_nome;
+        int quantidadeProduto, quantidadeCategoria, opcaoMenuInicial, opcaoSubMenu, categoriaNumero = 1, descontoContinuo, taxaEmergencial;
+        String produtoNome, categoriaNome;
+        double produtoPreco;
         boolean continua = true, controle = false, achou = false;
 
         Produto umProduto = null;
         Categoria umCategoria = null;
 
         System.out.println("Bem vindo a Lojinha do Bruno");
-        qnt_cat = Console1.readInt("\n Digite a quantidade de categorias que pretende cadastrar: ");
-        qnt_prod = Console1.readInt("Digite a quantidade de produtos que pretende cadastrar: ");
+        quantidadeCategoria = Console1.readInt("\nDigite a quantidade de categorias que pretende cadastrar: ");
+        quantidadeProduto = Console1.readInt("\nDigite a quantidade de produtos que pretende cadastrar: ");
 
-        ArrayList<Categoria> lista_de_categorias = new ArrayList(qnt_cat);
-        ArrayList<Produto> lista_de_produtos = new ArrayList(qnt_prod);
+        ArrayList<Categoria> lista_de_categorias = new ArrayList(quantidadeCategoria);
+        ArrayList<Produto> lista_de_produtos = new ArrayList(quantidadeProduto);
 
-        while(continua == true)
+        while(continua)
         {
             System.out.println("\nMenu de Ações");
             System.out.println("1 - Cadastrar.");
@@ -35,22 +35,43 @@ public class main {
             System.out.println("3 - Excluir.");
             System.out.println("4 - Listagem completa.");
             System.out.println("5 - Sair.");
-            opcao_menu = Console1.readInt("Digite o número (1 à 5) correspondente a ação escolhida: ");
 
-            switch (opcao_menu) {
+            opcaoMenuInicial = Console1.readInt("\nDigite o número (1 à 5) correspondente a ação escolhida: ");
+
+            switch (opcaoMenuInicial) {
                 case 1:
                     System.out.println("\nMenu de Cadastramento");
                     System.out.println("1 - Cadastrar nova categoria.");
                     System.out.println("2 - Cadastrar novo produto.");
-                    opcao = Console1.readInt("Digite o número (1 à 2) correspondente a ação escolhida: ");
 
-                    if(opcao == 1) {
+                    opcaoSubMenu = Console1.readInt("\nDigite o número (1 à 2) correspondente a ação escolhida: ");
+
+                    if((opcaoSubMenu == 1) && (quantidadeCategoria > 0)) {
                         System.out.println("\nCadastramento de Categoria");
-                        categoria_nome = Console1.readLine("Descrição da Categoria: ");
+                        System.out.println("Número da Categoria: " + categoriaNumero);
+                        categoriaNome = Console1.readLine("Descrição da Categoria: ");
 
-                        umCategoria = new Categoria(categoria_nome);
+                        umCategoria = new Categoria(categoriaNome);
                         lista_de_categorias.add(umCategoria);
-                        System.out.println("Categoria cadastrada com sucesso!");
+                        quantidadeCategoria --;
+                        categoriaNumero ++;
+
+                        System.out.println("\nCategoria cadastrada com sucesso!");
+                    }
+                    else {
+                        System.out.println("\nCadastramento de Produto");
+                        produtoNome = Console1.readLine("Nome do Produto: ");
+                        produtoPreco = Console1.readDouble("Preço do Produto: ");
+                        categoriaNumero = Console1.readInt("Número da Categoria: ");
+
+                        for(i = 0; i < lista_de_categorias.size(); ++i) {
+                            umCategoria = (Categoria)lista_de_categorias.get(i);
+                            if (umCategoria.getNumero() == categoria_numero) {
+                                achou = true;
+                                break;
+                            }
+                        }
+
                     }
 
             }
